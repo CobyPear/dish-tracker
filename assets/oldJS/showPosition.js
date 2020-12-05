e813a1666d8fa2205ad1077bc604c2b957d03369
_Pear <63015754+CobyPear@users.noreply.github.com>
2020-12-05 09:34:39 -0600

    removed old js

--- assets/oldJS/showPosition.js
+++
@@ -1,6 +0,0 @@
-function showPosition(position) {
-    var lat = position.coords.latitude;
-    var lon = position.coords.longitude;
-    latlongUrl = "https://us-restaurant-menus.p.rapidapi.com/restaurants/search/geo?page=" + page + "&lon=" + lon + "&lat=" + lat + "&distance=5"
-    localStorage.setItem('latLon', JSON.stringify(userLocationInfo))
-};
